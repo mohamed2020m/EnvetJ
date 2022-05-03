3fe9dfd450c16e7c286e4c1e8781b29cea0442e1
Mohamed ESSABIR <60801395+mohamed2020m@users.noreply.github.com>
2022-05-03 00:13:25 +0000

    Update contact.php

--- assets/contact.php
+++ assets/contact.php
@@ -24,15 +24,15 @@
         // Set the email subject.
         //$subject = "New contact from $name";
 
-        // Build the email content.
+        // Build the email headers.
+        $email_headers = "From: $name <$email>";
+
+         // Build the email content.
         $email_content = "First Name: $name\n";
         $email_content .= "Email: $email\n\n";
         $email_content .= "Subject: $subject\n\n";
         $email_content .= "Number: $number\n\n";
         $email_content .= "Message:\n$message\n";
-
-        // Build the email headers.
-        $email_headers = "From: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
