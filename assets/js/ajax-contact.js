93a42e09cdf3f2d7f42b8c53cf90071e10f35288
Mohamed ESSABIR <60801395+mohamed2020m@users.noreply.github.com>
2022-05-03 00:01:46 +0000

    updating ajax for contact form

--- assets/js/ajax-contact.js
+++ assets/js/ajax-contact.js
@@ -18,31 +18,30 @@ $(function() {
 		$.ajax({
 			url: $(form).attr('action'),
 			type: 'POST',
-			data: formData
-		})
-		.done(function(response) {
-			// Make sure that the formMessages div has the 'success' class.
-			$(formMessages).removeClass('error');
-			$(formMessages).addClass('success');
+			data: formData,
+			success: (function(response) {
+				// Make sure that the formMessages div has the 'success' class.
+				$(formMessages).removeClass('error');
+				$(formMessages).addClass('success');
 
-			// Set the message text.
-			$(formMessages).text(response);
+				// Set the message text.
+				$(formMessages).text(response);
 
-			// Clear the form.
-			$('#contact-form input,#contact-form textarea').val('');
-		})
-		.fail(function(data) {
-			// Make sure that the formMessages div has the 'error' class.
-			$(formMessages).removeClass('success');
-			$(formMessages).addClass('error');
+				// Clear the form.
+				$('#contact-form input,#contact-form textarea').val('');
+			}),
+			error: (function(data) {
+				// Make sure that the formMessages div has the 'error' class.
+				$(formMessages).removeClass('success');
+				$(formMessages).addClass('error');
 
-			// Set the message text.
-			if (data.responseText !== '') {
-				$(formMessages).text(data.responseText);
-			} else {
-				$(formMessages).text('Oops! An error occured and your message could not be sent.');
-			}
+				// Set the message text.
+				if (data.responseText !== '') {
+					$(formMessages).text(data.responseText);
+				} else {
+					$(formMessages).text('Oops! An error occured and your message could not be sent.');
+				}
+			})
 		});
 	});
-
 });
